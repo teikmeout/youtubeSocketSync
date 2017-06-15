4990855ab03e42d41fc6c9c90bbe5f96c2cb38ed
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-06-15 13:53:07 -0400

    added pause functionality and load with input bar

--- server.js
+++ server.js
@@ -26,20 +26,31 @@ server.listen(PORT, () => {
 const users = [];
 
 // socket io connection process
+// this is what happens when a user connects, a connection event happens
+// so we establish a connection listener.
 io.on('connection', function (socket) {
   console.log('new user connected: id -> ', socket.id);
   users.push(socket);
   console.log('users connected =', users.length);
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
   socket.on('ready', (data) => {
     console.log('video ready to play');
   });
   socket.on('play', (data) => {
-    console.log('playing video somewhere');
-    socket.broadcast.emit('play', {status: "playing"})
+    console.log('playing video somewhere', data);
+    socket.broadcast.emit('play', {status: "playing"});
+  })
+  socket.on('pause', (data) => {
+    console.log('video paused somewhere');
+    socket.broadcast.emit('pause', {status: "pausing"});
+  })
+
+  // this is gonna be the long switch statement
+  socket.on('statuschange', (data) => {
+
   })
 
 
